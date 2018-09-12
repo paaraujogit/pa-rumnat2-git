e0d1ad70c81fa1d61126b59cb8785822075a5c5d
RJLC1 <ricardo.j.l.capao@gmail.com>
2018-09-12 12:52:06 +0100

    ChangeName

--- src/pa/calc/RCOper.java
+++ src/pa/calc/RCOper.java
@@ -2,7 +2,7 @@ package pa.calc;
 
 import pa.calc.ItfOper;
 
-public class PAOperRICARDO implements ItfOper {
+public class RCOper implements ItfOper {
 
 	@Override
 	public double oper(double op1, double op2) {
