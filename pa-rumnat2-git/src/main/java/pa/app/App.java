167f0ac8a85e363e9d3b99d103cd2801857300bd
Mauricio Guedes Jr <guedesjr.mauricio@gmail.com>
2018-09-13 12:15:38 +0100

    Adicionando arquivos

---
+++ pa-rumnat2-git/src/main/java/pa/app/App.java
@@ -0,0 +1,13 @@
+package pa.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Exercício GIT Maven Jenkins" );
+    }
+}
