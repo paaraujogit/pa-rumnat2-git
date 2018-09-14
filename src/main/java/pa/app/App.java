8759900508fdf2873b61025ac4f9c797642a1c2d
Mauricio Guedes Jr. <guedesjr.mauricio@gmail.com>
2018-09-14 09:55:13 +0100

    Acicionando métodos de operações aritméticas

--- src/main/java/pa/app/App.java
+++ src/main/java/pa/app/App.java
@@ -1,5 +1,8 @@
 package pa.app;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 /**
  * Hello world!
  *
@@ -10,4 +13,54 @@ public class App
     {
     	System.out.println( mensagem );
     }
+    
+    public double somar( double valor1, double valor2 )
+    {
+    	return valor1 + valor2;
+    }
+    
+    public double subtrair( double valor1, double valor2 )
+    {
+    	return valor1 - valor2;
+    }
+    
+    public double multiplicar( double valor1, double valor2 )
+    {
+    	return truncar( valor1 * valor2, 2 );
+    }
+    
+    public double dividir( double valor1, double valor2 )
+    {
+    	return truncar( valor1 / valor2, 2 );
+    }
+    
+    public static double truncar( final double valor, final int decimais )
+	{
+		final NumberFormat numberFormat = NumberFormat.getNumberInstance( Locale.US );
+		numberFormat.setGroupingUsed( false );
+		numberFormat.setMinimumFractionDigits( decimais + 10 + 1 );
+		numberFormat.setMaximumFractionDigits( decimais + 10 + 1 );;
+		return truncar( valor, numberFormat , decimais );
+	}
+
+	public static double truncar( final double valor, final NumberFormat numberFormat, final int decimais )
+	{
+		if ( !Double.isNaN( valor ) && !Double.isInfinite( valor ) )
+		{
+			final String valorReplace = numberFormat.format( valor ).replace( ".", ";" );
+			final String[] valorString = valorReplace.split( ";" );
+
+			if ( valorString.length == 2 )
+			{
+				valorString[1] = valorString[1].concat( "000000000000000" );
+			}
+			else
+			{
+				valorString[1] = "000000000000000";
+			}
+
+			return Double.valueOf( valorString[0] + "." + valorString[1].substring( 0, decimais ) );
+		}
+		return valor;
+	}
 }
--- src/test/java/pa/app/AppTest.java
+++ src/test/java/pa/app/AppTest.java
@@ -13,17 +13,60 @@ class AppTest {
 	void testExecute() 
 	{
 		this.app.execute("####### Exercício GIT Maven Jenkins Avançado");
+		
+		assertTrue( true );
+	}
+	
+	@Test
+	void testSomar() 
+	{
+		double result = this.app.somar( 10, 20 );
+		
+		assertEquals( 30, result );
+		
+		System.out.println( "####### Teste de soma realizado com sucesso." ); 
+	}
+	
+	@Test
+	void testSubtrair()
+	{
+		double result = this.app.subtrair( 50, 30 );
+		
+		assertEquals( 20, result );
+		
+		System.out.println( "####### Teste de subtração realizado com sucesso." ); 
+	}
+	
+	@Test
+	void testMultiplacacar()
+	{
+		double result = this.app.multiplicar( 5, 3 );
+		
+		assertEquals( 15, result );
+		
+		System.out.println( "####### Teste de multiplicação realizado com sucesso." ); 
+	}
+	
+	@Test
+	void testDividir()
+	{
+		double result = this.app.dividir( 100, 4 );
+		
+		assertEquals( 25, result );
+		
+		System.out.println( "####### Teste de subtração realizado com sucesso." ); 
 	}
 
 	@BeforeAll
     static void beforeAll() 
 	{
-        System.out.println("Before all test methods");
+        System.out.println("####### Before all test methods");
     }
 	
 	@BeforeEach
 	void beforeEach()
 	{
 		this.app = new App();
+		System.out.println("####### Before Each test methods");
 	}	
 }
